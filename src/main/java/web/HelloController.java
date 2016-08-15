0abfcd22353811e7898001198d17f3ad835ceace
charlie <1151832099@qq.com>
2016-08-15 14:01:56 +0800

    init version

---
+++ src/main/java/web/HelloController.java
@@ -0,0 +1,25 @@
+package web;
+
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/hello")
+public class HelloController implements EnvironmentAware {
+
+    @RequestMapping("/world")
+    public @ResponseBody String hello() {
+        System.out.println("====hello");
+        return "hello";
+    }
+
+    private Environment environment = null;
+
+    @Override
+    public void setEnvironment(Environment arg0) {
+        this.environment = arg0;
+    }
+}
