1df4afdfa8d8b7350103edfac9443887af090c95
charlie <1151832099@qq.com>
2016-08-23 16:17:12 +0800

    responseBodyAdvice

---
+++ src/main/java/web/ResponseBodyAdviceController.java
@@ -0,0 +1,29 @@
+package web;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class ResponseBodyAdviceController
+        implements ResponseBodyAdvice<String> {
+
+    @Override
+    public boolean supports(MethodParameter returnType,
+            Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public String beforeBodyWrite(String body, MethodParameter returnType,
+            MediaType selectedContentType,
+            Class<? extends HttpMessageConverter<?>> selectedConverterType,
+            ServerHttpRequest request, ServerHttpResponse response) {
+        return body + "this is been modified";
+    }
+
+}
