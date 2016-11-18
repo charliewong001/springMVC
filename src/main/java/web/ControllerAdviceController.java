13461c56ba1d55f3aff17d1d40fc26d4226541fd
charlie <1151832099@qq.com>
2016-11-18 17:13:42 +0800

    a

--- src/main/java/web/ControllerAdviceController.java
+++ src/main/java/web/ControllerAdviceController.java
@@ -3,6 +3,12 @@ package web;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+/**
+ * 配置了@ControllerAdvice的类中的@ModelAttribute和@InitBinder方法会按照规则统一执行
+ * 
+ * @author Charlie
+ *
+ */
 @ControllerAdvice
 public class ControllerAdviceController {
     @ModelAttribute
--- src/main/java/web/ResponseBodyAdviceController.java
+++ src/main/java/web/ResponseBodyAdviceController.java
@@ -8,6 +8,14 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+/**
+ * 实现了ResponseBodyAdvice接口可以处理修改作为ResponseBody类型的处理器的返回值
+ * 会将返回值作为ResponseBody的两种类型的处理器为:返回职位HttpEntity,返回值前注释了@ResponseBody
+ * 主要用于修改json类型的返回值,可以在里边统一处理
+ * 
+ * @author Charlie
+ *
+ */
 @ControllerAdvice
 public class ResponseBodyAdviceController
         implements ResponseBodyAdvice<String> {
